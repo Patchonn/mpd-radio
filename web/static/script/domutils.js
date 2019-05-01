17ae6fe328ad901fcbb78c3d9e6793234965500a
patchon <patchon@myon.moe>
2019-05-01 22:57:10 +0100

    missing file

---
+++ web/static/script/domutils.js
@@ -0,0 +1,99 @@
+
+// dom utils
+let du = {};
+
+(function(du){
+    
+    class Element {
+        constructor(element, parent) {
+            this.parent_ = (parent !== undefined) ? parent : null;
+            
+            this.element = element;
+            this.doc = element.ownerDocument;
+        }
+        
+        attr(name, value) {
+            if(value === null)
+                this.element.removeAttribute(name);
+            else
+                this.element.setAttribute(name, value);
+            
+            return this;
+        }
+        data(name, value) {
+            if(value === null)
+                delete this.element.dataset[name]
+            else
+                this.element.dataset[name] = value;
+            
+            return this;
+        }
+        style(name, value) {
+            name = name.replace(/-(.)/g, (e) => e[1].toUpperCase())
+            this.element.style[name] = value;
+            return this;
+        }
+        toggleClass(name, add) {
+            this.element.classList.toggle(name, add);
+            return this;
+        }
+        text(txt) {
+            this.element.textContent = txt;
+            return this;
+        }
+        listen(name, fun) {
+            this.element.addEventListener(name, (e) => {
+                return fun.call(this, e, this);
+            });
+            return this;
+        }
+        
+        parent() {
+            return this.parent_;
+        }
+        
+        append(tag){
+            let child = this.doc.createElement(tag);
+            this.element.appendChild(child);
+            return new Element(child, this);
+        }
+        appendChild(tag){
+            let child = this.doc.createElement(tag);
+            this.element.appendChild(child);
+            return this;
+        }
+        remove() {
+            this.element.parentNode.removeChild(this.element);
+        }
+        
+        call(fun) {
+            fun(this);
+            return this;
+        }
+        
+        select(query) {
+            let selection = this.element.querySelector(query);
+            if(selection === null) return null;
+            return new Element(selection);
+        }
+    };
+    
+    du.select = function(query, doc) {
+        doc = (doc !== undefined) ? doc : document;
+        if(query.nodeType > 0) {
+            return new Element(query);
+        } else {
+            let selection = doc.querySelector(query);
+            if(selection === null) return null;
+            return new Element(selection);
+        }
+    };
+    du.id = function(id, doc) {
+        doc = (doc !== undefined) ? doc : document;
+        let element = doc.getElementById(id);
+        if(element === null) return null;
+        return new Element(element);
+    };
+    
+})(du);
+
